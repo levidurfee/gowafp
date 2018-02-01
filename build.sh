9c63c60b3f0444d47d7012b78dae27c2f8ed721f
Levi Durfee <levi.durfee@gmail.com>
2018-02-01 13:02:44 -0500

    remove Docker files for now

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -f Docker/nginx/Dockerfile -t levidurfee/gowafp_nginx:latest .
-docker build -f Docker/Go/Dockerfile -t levidurfee/gowafp_fw:latest .
-docker build -f Docker/php/Dockerfile -t levidurfee/gowafp_php:latest .
-
-docker push levidurfee/gowafp_nginx:latest
-docker push levidurfee/gowafp_fw:latest
-docker push levidurfee/gowafp_php:latest
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker stack deploy -c docker-compose.yml --with-registry-auth gowafp
