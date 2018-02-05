f2beadb38a25896d099d4264ec47339add3b2f57
Levi Durfee <levi.durfee@gmail.com>
2018-02-05 13:09:12 -0500

    Run go fmt

--- gowafp_test.go
+++ gowafp_test.go
@@ -1,22 +1,22 @@
 package gowafp
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
+	"testing"
 )
 
 func TestPhpHandler(t *testing.T) {
-    phpHandler := PhpHandler("/app/index.php", "tcp", "127.0.0.1:9000")
-    _, ok := phpHandler.(http.Handler)
-    if !ok {
-        t.Error("Not a http handler")
-    }
+	phpHandler := PhpHandler("/app/index.php", "tcp", "127.0.0.1:9000")
+	_, ok := phpHandler.(http.Handler)
+	if !ok {
+		t.Error("Not a http handler")
+	}
 }
 
 func TestAnalyzeRequest(t *testing.T) {
-    handler := AnalyzeRequest(PhpHandler("/app/index.php", "tcp", "127.0.0.1:9000"))
-    _, ok := handler.(http.Handler)
-    if !ok {
-        t.Error("Not a http handler")
-    }
+	handler := AnalyzeRequest(PhpHandler("/app/index.php", "tcp", "127.0.0.1:9000"))
+	_, ok := handler.(http.Handler)
+	if !ok {
+		t.Error("Not a http handler")
+	}
 }
