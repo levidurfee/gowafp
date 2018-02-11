dd834f7b8d3c0d035db68522b272eb7045a02f17
Levi Durfee <levi.durfee@gmail.com>
2018-02-11 17:34:22 -0500

    test for sql injection (#11) (#12)

--- gowafp_test.go
+++ gowafp_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+func TestPossibleSqlInjection(t *testing.T) {
+	result := possibleSqlInjection("1'or'1'='1")
+	if result != true {
+		t.Error("Possible SQL Injection failed")
+	}
+}
+
 func TestPhpHandler(t *testing.T) {
 	phpHandler := PhpHandler("/app/index.php", "tcp", "127.0.0.1:9000")
 	_, ok := phpHandler.(http.Handler)
